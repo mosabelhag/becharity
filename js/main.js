ccfb565761e46b15c8a239db6bd054afa5c10d15
Mousab Elhag <mosabelhag@gmail.com>
2018-03-02 16:20:30 -0800

    Adding slidwe mainmenu & fact section

--- js/main.js
+++ js/main.js
@@ -5,8 +5,37 @@ Main js
 
 $(document).ready(function() {
 
-	setTimeout(function(){
-		$('body').addClass('loaded');
-	}, 3000);
+  setTimeout(function() {
+    $('body').addClass('loaded');
+  }, 3000);
 
+
+  $("#owl-demo").owlCarousel({
+
+    navigation: false, // Show next and prev buttons
+    slideSpeed: 2000,
+    paginationSpeed: 400,
+    items: 1,
+    autoplay: true,
+    loop: true,
+    responsiveRefreshRate: 200,
+    stopOnHover:true,
+
+    // "singleItem:true" is a shortcut for:
+    // items : 1,
+    // itemsDesktop : false,
+    // itemsDesktopSmall : false,
+    // itemsTablet: false,
+    // itemsMobile : false
+
+  });
+
+  $(window).scroll(function() {
+      var menu = $('.main-menu');
+      if ($(window).scrollTop() >= menu.height()) {
+        menu.addClass('navbar-fixed-top fixed-scroll');
+      } else {
+        menu.removeClass('navbar-fixed-top fixed-scroll');
+      }
+    });
 });
